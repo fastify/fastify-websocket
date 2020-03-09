55a20283a4abcd6363cfb17145f6a192fe7cd8fa
Edvards Lazdāns <edvards@lazdans.lv>
2020-03-09 13:01:41 +0200

    Add websocketServer type (#51)
    * Add websocketServer type
    
    * Add TS test for app.websocketServer

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -3,6 +3,7 @@ const fastify = require('fastify');
 import { SocketStream } from '../..';
 import { WebsocketHandler, FastifyRequest, FastifyInstance } from 'fastify';
 import { expectType } from 'tsd';
+import { Server } from 'ws';
 
 const app: FastifyInstance = fastify();
 app.register(wsPlugin);
@@ -13,6 +14,7 @@ const handler: WebsocketHandler = (
   params
 ) => {
   expectType<SocketStream>(connection);
+  expectType<Server>(app.websocketServer);
   expectType<{ [key: string]: any } | undefined>(params);
 };
 
