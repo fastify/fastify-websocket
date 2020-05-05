9d0cf8c0453c9a7c67085dfbc5c12182e039484b
Peter Balazs <frikille@gmail.com>
2020-05-05 11:12:14 +0100

    update fastify to v3 (#54)
    * update fastify to v3
    
    * add dependabot config
    
    * move ci to github actions
    
    * update dependabot config

--- index.d.ts
+++ index.d.ts
@@ -1,98 +1,50 @@
 /// <reference types="node" />
 import { IncomingMessage, ServerResponse, Server } from 'http';
-import { Plugin } from 'fastify';
+import { FastifyPlugin, FastifyRequest, RawServerBase, RawServerDefault, RawRequestDefaultExpression, RawReplyDefaultExpression, RequestGenericInterface, ContextConfigDefault } from 'fastify';
 import * as WebSocket from 'ws';
 import { Duplex } from 'stream';
 
 declare module 'fastify' {
   interface RouteShorthandOptions<
-    HttpServer,
-    HttpRequest,
-    HttpResponse,
-    Query,
-    Params,
-    Headers,
-    Body
+    RawServer extends RawServerBase = RawServerDefault
   > {
     websocket?: boolean;
   }
-  interface FastifyInstance<
-    HttpServer = Server,
-    HttpRequest = IncomingMessage,
-    HttpResponse = ServerResponse
-  > {
-    get<
-      Query = DefaultQuery,
-      Params = DefaultParams,
-      Headers = DefaultHeaders,
-      Body = DefaultBody
-    >(
-      url: string,
-      opts: RouteShorthandOptions<
-        HttpServer,
-        HttpRequest,
-        HttpResponse,
-        Query,
-        Params,
-        Headers,
-        Body
-      >,
-      handler?: WebsocketHandler<HttpRequest, HttpResponse>
-    ): FastifyInstance<HttpServer, HttpRequest, HttpResponse>;
 
-    websocketServer: WebSocket.Server;
+  interface FastifyInstance<RawServer, RawRequest, RawReply> {
+    get: RouteShorthandMethod<RawServer, RawRequest, RawReply>
+    websocketServer: WebSocket.Server,
   }
 
-  interface RouteOptions<
-    HttpServer = Server,
-    HttpRequest = IncomingMessage,
-    HttpResponse = ServerResponse,
-    Query = DefaultQuery,
-    Params = DefaultParams,
-    Headers = DefaultHeaders,
-    Body = DefaultBody
+  interface RouteShorthandMethod<
+    RawServer extends RawServerBase = RawServerDefault,
+    RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+    RawReply extends RawReplyDefaultExpression<RawServer> = RawReplyDefaultExpression<RawServer>,
   > {
-    wsHandler?: WebsocketHandler<
-      HttpRequest,
-      HttpResponse,
-      Query,
-      Params,
-      Headers,
-      Body
-    >;
+    <RequestGeneric extends RequestGenericInterface = RequestGenericInterface, ContextConfig = ContextConfigDefault>(
+      path: string,
+      opts: RouteShorthandOptions<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>,
+      handler?: WebsocketHandler
+    ): FastifyInstance<RawServer, RawRequest, RawReply>;
   }
 
-  export type WebsocketHandler<
-    HttpRequest = IncomingMessage,
-    HttpResponse = ServerResponse,
-    Query = DefaultQuery,
-    Params = DefaultParams,
-    Headers = DefaultHeaders,
-    Body = DefaultBody
-  > = (
-    this: FastifyInstance<Server, HttpRequest, HttpResponse>,
-    connection: websocketPlugin.SocketStream,
-    request: FastifyRequest<HttpRequest, Query, Params, Headers, Body>,
+  export type WebsocketHandler = (
+    this: FastifyInstance<Server, IncomingMessage, ServerResponse>,
+    connection: SocketStream,
+    request: FastifyRequest,
     params?: { [key: string]: any }
   ) => void | Promise<any>;
 }
 
-declare namespace websocketPlugin {
-  export interface SocketStream extends Duplex {
-    socket: WebSocket;
-  }
-
-  export interface PluginOptions {
-    handle?: (connection: SocketStream) => void;
-    options?: WebSocket.ServerOptions;
-  }
+export interface SocketStream extends Duplex {
+  socket: WebSocket;
 }
 
-declare const websocketPlugin: Plugin<
-  Server,
-  IncomingMessage,
-  ServerResponse,
-  websocketPlugin.PluginOptions
->;
+export interface WebsocketPluginOptions {
+  handle?: (connection: SocketStream) => void;
+  options?: WebSocket.ServerOptions;
+}
 
-export = websocketPlugin;
+declare const websocketPlugin: FastifyPlugin<WebsocketPluginOptions>;
+
+export default websocketPlugin;
