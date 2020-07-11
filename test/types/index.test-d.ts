bdb94921c85c7b99f8822ea8597d7b1124662fa8
einnjo <juanjo.lenero@gmail.com>
2020-07-10 19:01:21 -0500

    test exposed server type in handlers

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -4,25 +4,25 @@ import { expectType } from 'tsd';
 import { Server as HttpServer, IncomingMessage } from 'http'
 import { Server } from 'ws';
 
-const handler: WebsocketHandler = (
+const app: FastifyInstance = fastify();
+app.register(wsPlugin);
+app.register(wsPlugin, {});
+app.register(wsPlugin, {
+  handle: function globalHandler(connection: SocketStream): void {
+    expectType<FastifyInstance>(this);
+    expectType<SocketStream>(connection)
+  }
+});
+app.register(wsPlugin, { options: { perMessageDeflate: true } });
+
+app.get('/', { websocket: true }, function perRouteHandler(
   connection: SocketStream,
   req: IncomingMessage,
   params
-) => {
+) {
+  expectType<FastifyInstance>(this);
   expectType<SocketStream>(connection);
   expectType<Server>(app.websocketServer);
   expectType<IncomingMessage>(req)
   expectType<{ [key: string]: any } | undefined>(params);
-};
-
-const handle = (connection: SocketStream): void => {
-  expectType<SocketStream>(connection)
-}
-
-const app: FastifyInstance = fastify();
-app.register(wsPlugin);
-app.register(wsPlugin, {});
-app.register(wsPlugin, { handle } );
-app.register(wsPlugin, { options: { perMessageDeflate: true } });
-
-app.get('/', { websocket: true }, handler);
+});
