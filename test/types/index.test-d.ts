fastify/fastify-websocket

commit 0838d439acea3f85d2fd6d9fd22d5cc9b0b96e2b

Update TypeScript types to match actual request object passed to handlers (#64)
In the README it states (and in my experience also) the handler doesn't get a full on `FastifyRequest` object. It gets an `http.IncomingMessage` instead! This makes the TypeScript types reflect that.

index.d.ts
@@ -31,7 +31,7 @@ declare module 'fastify' {
   export type WebsocketHandler = (
     this: FastifyInstance<Server, IncomingMessage, ServerResponse>,
     connection: SocketStream,
-    request: FastifyRequest,
+    request: IncomingMessage,
     params?: { [key: string]: any }
   ) => void | Promise<any>;
 }

test/types/index.test-d.ts
@@ -6,18 +6,18 @@ import { Server } from 'ws';
 
 const handler: WebsocketHandler = (
   connection: SocketStream,
-  req: FastifyRequest,
+  req: IncomingMessage,
   params
 ) => {
   expectType<SocketStream>(connection);
   expectType<Server>(app.websocketServer);
-  expectType<FastifyRequest<HttpServer, IncomingMessage, RequestGenericInterface>>(req)
+  expectType<IncomingMessage>(req)
   expectType<{ [key: string]: any } | undefined>(params);
 };
 
 const handle = (connection: SocketStream): void => {
   expectType<SocketStream>(connection)
-} 
+}
 
 const app: FastifyInstance = fastify();
 app.register(wsPlugin);

commit b0589a64f6662134e3e510d150450de6993ee7e8

Ensure websocket handler types are only applied to websocket handlers
This corrects an issue introduced in #64 where upon adding `fastify-websocket` to a project all route handlers were (accidentally) assumed to be websocket handlers getting the different (and decidedly less useful) types. My bad! This corrects the issue by using a type-land overload of the RouteShorthand function definition to change the type of the handler only if the handler is in fact `{ websocket: true }`.

Also adds tests, `tsd` is handy!

index.d.ts
@@ -23,7 +23,7 @@ declare module 'fastify' {
   > {
     <RequestGeneric extends RequestGenericInterface = RequestGenericInterface, ContextConfig = ContextConfigDefault>(
       path: string,
-      opts: RouteShorthandOptions<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig>,
+      opts: RouteShorthandOptions<RawServer, RawRequest, RawReply, RequestGeneric, ContextConfig> & { websocket: true }, // this creates an overload that only applies these different types if the handler is for websockets
       handler?: WebsocketHandler
     ): FastifyInstance<RawServer, RawRequest, RawReply>;
   }

test/types/index.test-d.ts
@@ -1,5 +1,5 @@
 import wsPlugin, { SocketStream } from '../..';
-import fastify, { WebsocketHandler, FastifyRequest, FastifyInstance, RequestGenericInterface } from 'fastify';
+import fastify, { WebsocketHandler, FastifyRequest, FastifyInstance, RequestGenericInterface, FastifyReply } from 'fastify';
 import { expectType } from 'tsd';
 import { Server as HttpServer, IncomingMessage } from 'http'
 import { Server } from 'ws';
@@ -7,6 +7,7 @@ import { Server } from 'ws';
 const app: FastifyInstance = fastify();
 app.register(wsPlugin);
 app.register(wsPlugin, {});
+app.register(wsPlugin, { options: { maxPayload: 123 } });
 app.register(wsPlugin, {
   handle: function globalHandler(connection: SocketStream): void {
     expectType<FastifyInstance>(this);
@@ -15,14 +16,29 @@ app.register(wsPlugin, {
 });
 app.register(wsPlugin, { options: { perMessageDeflate: true } });
 
-app.get('/', { websocket: true }, function perRouteHandler(
-  connection: SocketStream,
-  req: IncomingMessage,
-  params
-) {
+app.get('/websockets-via-inferrence', { websocket: true }, async function(connection, req, params) {
   expectType<FastifyInstance>(this);
   expectType<SocketStream>(connection);
   expectType<Server>(app.websocketServer);
   expectType<IncomingMessage>(req)
   expectType<{ [key: string]: any } | undefined>(params);
+});
+
+const handler: WebsocketHandler = async (connection, req, params) => {
+  expectType<SocketStream>(connection);
+  expectType<Server>(app.websocketServer);
+  expectType<IncomingMessage>(req)
+  expectType<{ [key: string]: any } | undefined>(params);
+}
+
+app.get('/websockets-via-annotated-const', { websocket: true }, handler);
+
+app.get('/not-specifed', async (request, reply) => {
+  expectType<FastifyRequest>(request);
+  expectType<FastifyReply>(reply)
+});
+
+app.get('/not-websockets', { websocket: false }, async (request, reply) => {
+  expectType<FastifyRequest>(request);
+  expectType<FastifyReply>(reply);
 });