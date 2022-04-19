04b938e35d2447823c9a8078638b33ff137c5f49
Vincent LE GOFF <vince.legoff@gmail.com>
2022-04-19 14:55:49 +0200

    feat: add message validation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const kWsHead = Symbol('ws-head')
 
 function fastifyWebsocket (fastify, opts, next) {
   fastify.decorateRequest('ws', null)
-
+  
   let errorHandler = defaultErrorHandler
   if (opts.errorHandler) {
     if (typeof opts.errorHandler !== 'function') {
@@ -22,29 +22,29 @@ function fastifyWebsocket (fastify, opts, next) {
   if (opts.options && opts.options.noServer) {
     return next(new Error("fastify-websocket doesn't support the ws noServer option. If you want to create a websocket server detatched from fastify, use the ws library directly."))
   }
-
+  
   const wssOptions = Object.assign({ noServer: true }, opts.options)
-
+  
   if (wssOptions.path) {
     fastify.log.warn('ws server path option shouldn\'t be provided, use a route instead')
   }
-
+  
   // We always handle upgrading ourselves in this library so that we can dispatch through the fastify stack before actually upgrading
   // For this reason, we run the WebSocket.Server in noServer mode, and prevent the user from passing in a http.Server instance for it to attach to.
   // Usually, we listen to the upgrade event of the `fastify.server`, but we do still support this server option by just listening to upgrades on it if passed.
   const websocketListenServer = wssOptions.server || fastify.server
   delete wssOptions.server
-
+  
   const wss = new WebSocket.Server(wssOptions)
   fastify.decorate('websocketServer', wss)
-
+  
   websocketListenServer.on('upgrade', (rawRequest, socket, head) => {
     // Save a reference to the socket and then dispatch the request through the normal fastify router so that it will invoke hooks and then eventually a route handler that might upgrade the socket.
     rawRequest[kWs] = socket
     rawRequest[kWsHead] = head
-
+    
     if (closing) {
-      handleUpgrade(rawRequest, (connection) => {
+      handleUpgrade(rawRequest, request, (connection) => {
         connection.socket.close(1001)
       })
     } else {
@@ -53,12 +53,14 @@ function fastifyWebsocket (fastify, opts, next) {
       fastify.routing(rawRequest, rawResponse)
     }
   })
-
-  const handleUpgrade = (rawRequest, callback) => {
+  
+  const handleUpgrade = (rawRequest, request, callback) => {
     wss.handleUpgrade(rawRequest, rawRequest[kWs], rawRequest[kWsHead], (socket) => {
       wss.emit('connection', socket, rawRequest)
-
       const connection = WebSocket.createWebSocketStream(socket, opts.connectionOptions)
+      socket.afterDuplex = true
+      socket.validator = request.context.schema ? fastify.validatorCompiler({ schema: request.context.schema.body }) : null
+      socket.strict = opts.strictMode ? opts.strictMode : false
       connection.socket = socket
 
       connection.socket.on('newListener', event => {
@@ -66,11 +68,11 @@ function fastifyWebsocket (fastify, opts, next) {
           connection.resume()
         }
       })
-
+      
       callback(connection)
     })
   }
-
+  
   fastify.addHook('onRequest', (request, reply, done) => { // this adds req.ws to the Request object
     if (request.raw[kWs]) {
       request.ws = true
@@ -84,14 +86,14 @@ function fastifyWebsocket (fastify, opts, next) {
     if (request.raw[kWs]) {
       // Hijack reply to prevent fastify from sending the error after onError hooks are done running
       reply.hijack()
-      handleUpgrade(request.raw, connection => {
+      handleUpgrade(request.raw, request, connection => {
         // Handle the error
         errorHandler.call(this, error, connection, request, reply)
       })
     }
     done()
   })
-
+  
   fastify.addHook('onRoute', routeOptions => {
     let isWebsocketRoute = false
     let wsHandler = routeOptions.wsHandler
@@ -101,9 +103,9 @@ function fastifyWebsocket (fastify, opts, next) {
       if (routeOptions.method !== 'GET') {
         throw new Error('websocket handler can only be declared in GET method')
       }
-
+      
       isWebsocketRoute = true
-
+      
       if (routeOptions.websocket) {
         wsHandler = routeOptions.handler
         handler = function (request, reply) {
@@ -122,7 +124,7 @@ function fastifyWebsocket (fastify, opts, next) {
       // within the route handler, we check if there has been a connection upgrade by looking at request.raw[kWs]. we need to dispatch the normal HTTP handler if not, and hijack to dispatch the websocket handler if so
       if (request.raw[kWs]) {
         reply.hijack()
-        handleUpgrade(request.raw, connection => {
+        handleUpgrade(request.raw, request, connection => {
           let result
           try {
             if (isWebsocketRoute) {
@@ -145,9 +147,9 @@ function fastifyWebsocket (fastify, opts, next) {
   })
 
   fastify.addHook('onClose', close)
-
+  
   let closing = false
-
+  
   // Fastify is missing a pre-close event, or the ability to
   // add a hook before the server.close call. We need to resort
   // to monkeypatching for now.
@@ -158,19 +160,19 @@ function fastifyWebsocket (fastify, opts, next) {
     // Call oldClose first so that we stop listening. This ensures the
     // server.clients list will be up to date when we start closing below.
     oldClose.call(this, cb)
-
+    
     const server = fastify.websocketServer
     if (!server.clients) return
     for (const client of server.clients) {
       client.close()
     }
   }
-
+  
   function noHandle (connection, rawRequest) {
     this.log.info({ path: rawRequest.url }, 'closed incoming websocket connection for path with no websocket handler')
     connection.socket.close()
   }
-
+  
   function defaultErrorHandler (error, conn, request, reply) {
     // Before destroying the connection, we attach an error listener.
     // Since we already handled the error, adding this listener prevents the ws
@@ -191,7 +193,7 @@ function fastifyWebsocket (fastify, opts, next) {
       return oldDefaultRoute(req, res)
     }
   })
-
+  
   next()
 }
 
@@ -200,7 +202,48 @@ function close (fastify, done) {
   server.close(done)
 }
 
+class ValidateWebSocket extends WebSocket {
+  constructor (...args) {
+    super(...args)
+    this.afterDuplex = false
+    this.validator = null
+    this.strict = false
+  }
+
+  wrapValidation (handler) {
+    return (message, isBinary) => {
+      if (isBinary) return handler(message, isBinary)
+      try {
+        if (!this.validator(JSON.parse(message.toString()))) {
+          if (this.strict) {
+            return this.close(1003, 'Unsupported payload')
+          } else {
+            return this.send(JSON.stringify(this.validator.errors))
+          }
+        }
+      } catch (e) {
+        if (this.strict) {
+          return this.close(1003, 'Unsupported payload')
+        } else {
+          return this.send('Unsupported payload')
+        }
+      }
+      return handler(message, isBinary)
+    }
+  }
+
+  on (event, handler, options) {
+    if (!this.afterDuplex) return super.on(event, handler)
+    if (event === 'message') {
+      super.on(event, this.wrapValidation(handler), options)
+    } else {
+      super.on(event, handler, options)
+    }
+  }
+}
+
 module.exports = fp(fastifyWebsocket, {
   fastify: '>= 3.11.0',
   name: 'fastify-websocket'
 })
+module.exports.ValidateWebSocket = ValidateWebSocket
